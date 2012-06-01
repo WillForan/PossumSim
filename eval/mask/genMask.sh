6bead8ea88aa280d704befdfa71f00b3e22c5a66
Will Foran <will.foran@gmail.com>
2012-06-01 09:07:22 -0400

    results to readme

--- eval/mask/genMask.sh
+++ eval/mask/genMask.sh
@@ -10,11 +10,19 @@ function demeanPSC {
 
    # bring 3mm sim output (as flirt input) into simulation input 1 mm (as flirt ref) 
    # so the sim input can be used to mask the sim output
-   [ -r ${simName}_to_mniRPI.nii.gz ] ||                              \
-    flirt -in $simOutput -ref $simBrain -out ${simName}_to_mniRPI     \
-          -dof 6 -schedule ${FSLDIR}/etc/flirtsch/sch3Dtrans_3dof       \
+   if [ ! -r ${simName}_to_mniRPI.nii.gz ] ; then
+    flirt -in $simOutput -ref $simBrain -out ${simName}_to_mniRPI_pre \
+          -omat func_to_mniRPI.mat                                    \
+          -dof 6 -schedule ${FSLDIR}/etc/flirtsch/sch3Dtrans_3dof     \
           -interp sinc -sincwidth 7 -sincwindow hanning
-
+    applywarp \
+          --ref=$simBrain \
+          --in=$simOutput \
+          --out=${simName}_to_mniRPI \
+          --premat=func_to_mniRPI.mat \
+          --interp=sinc
+   fi
+   # *** WHY: dim t == 1!?  _to_mniRPI
 
    # get all non zero voxels in one place (flatten time series)
    # this takes too long, but no danger of messing up orientation 
@@ -27,20 +35,28 @@ function demeanPSC {
              -expr 'step(abs(a))*b' -prefix ${simName}_MaskByInput.nii.gz
 
    # get PSC with mean= 1.0
-   if [ ! -r  ${simName}_scale1.nii.gz ]; then
+   #PSC=${simName}_scale1.nii.gz
+   PSC=${simName}_PSC.nii.gz
+   if [ ! -r  $PSC ]; then
 
       # grand mean intensity scaling factor to achieve M = 100
+      #fslmaths ${simName}_MaskByInput.nii.gz -add 100 -ing 100 ${simName}_scaleM100 -odt float
+      # -inm <mean> :  (-i i ip.c) intensity normalisation (per 3D volume mean)
+      # -ing <mean> :  (-I i ip.c) intensity normalisation, global 4D mean)
+      # or use -Tmean and subtract new from orig.
       # actually want voxel mean though ???
-      fslmaths ${simName}_MaskByInput.nii.gz -add 100 -ing 100 ${simName}_scaleM100 -odt float
+      #fslmaths ${simName}_scaleM100 -div 100 ${PSC} -odt float
 
-      #dividing the M=100 file by 100 yields a proportion of mean scaling (PSC)
-      fslmaths ${simName}_scaleM100 -div 100 ${simName}_scale1 -odt float
+      # get mean over T
+      fslmaths ${simName}_MaskByInput.nii.gz -Tmean ${simName}_Tmean -odt float
+      # (subtract from actual (mean at 1) - 1) * 100
+      3dcalc -i ${simName}_MaskByInput.nii.gz -m ${simName}_Tmean.nii.gz -expr '((i-m)/m)*100' -prefix ${PSC}
    fi 
 
    set +xe
 
    # print some stats
-   fslstats ${simName}_scale1.nii.gz   $(seq 0 10 100|sed 's/^/-P /')
+   fslstats ${PSC}   $(seq 0 10 100|sed 's/^/-P /')
 }
 
  ourInput="../../runPossum/inputs/10653_POSSUM4D_bb244_fullFreq_RPI.nii.gz"
